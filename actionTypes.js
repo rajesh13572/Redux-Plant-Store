46b28aad2adb5b1461d27a4edd496738dd7e7c51
PANIGRAHI RAJESH <2400090267@kluniversity.in>
2025-10-03 08:52:22 +0530

    Create actionTypes.js

---
+++ actionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_ITEM = 'ADD_ITEM';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const INCREASE_QUANTITY = 'INCREASE_QUANTITY';
+export const DECREASE_QUANTITY = 'DECREASE_QUANTITY';
+export const DELETE_ITEM = 'DELETE_ITEM';
